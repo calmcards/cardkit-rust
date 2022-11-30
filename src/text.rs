971e2b70c1f7d80adf10c027d7cf5b45aedfeb03
⚫️ <jp@bultmann.eu>
2022-11-30 10:04:38 +0100

    add bytemap to text drawing utils

--- src/text.rs
+++ src/text.rs
@@ -85,3 +85,28 @@ pub fn rect(text: &mut TextBuffer, top_left: (f32, f32), inner_width: usize, inn
         text.set(p.into(), style.left);
     }
 }
+
+pub fn bytemap<F>(text: &mut TextBuffer, top_left: (f32, f32), mut f: F)
+where
+    F: FnMut(u8) -> char
+{
+    label_with_stride(text, (Vec2::from(top_left) + (3.0, 0.0).into()).into(), (1.0, 0.0), String::from("0123456789ABCDEF"));
+    label_with_stride(text, (Vec2::from(top_left) + (0.0, 2.0).into()).into(), (0.0, 1.0), String::from("01234567"));
+    label_with_stride(text, (Vec2::from(top_left) + (1.0, 2.0).into()).into(), (0.0, 1.0), String::from("________"));
+    label_with_stride(text, (Vec2::from(top_left) + (23.0, 0.0).into()).into(), (1.0, 0.0), String::from("0123456789ABCDEF"));
+    label_with_stride(text, (Vec2::from(top_left) + (20.0, 2.0).into()).into(), (0.0, 1.0), String::from("01234567"));
+    label_with_stride(text, (Vec2::from(top_left) + (21.0, 2.0).into()).into(), (0.0, 1.0), String::from("________"));
+    rect(text, (Vec2::from(top_left) + (2.0, 1.0).into()).into(), 16, 8, RectStyle::THIN);
+    rect(text, (Vec2::from(top_left) + (22.0, 1.0).into()).into(), 16, 8, RectStyle::THIN);
+
+    for byte in 0u8..=255u8 {
+        let x: f32 = if byte < 128 {
+            (byte & 0b1111) as f32 + 3.0
+        } else {
+            (byte & 0b1111) as f32 + 23.0
+        };
+        let y: f32 = ((byte >> 4) & 0b111) as f32 + 2.0;
+
+        text.set((Vec2::from(top_left) + (x, y).into()).into(), f(byte));
+    }
+}
